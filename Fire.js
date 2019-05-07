495c4a1ecdc4c968b5f85f64e4260e974a2a0a87
Diego <diegosouza_pba@hotmail.com>
2019-05-07 08:08:54 -0300

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -1,4 +1,84 @@
-class Fire {}
+import firebase from 'firebase'; // 4.8.1
+
+class Fire {
+  constructor() {
+    this.init();
+    this.observeAuth();
+  }
+
+  init = () =>
+    firebase.initializeApp({
+    apiKey: "AIzaSyCd1wKVYsjM54XH9TevMTqOE7ixwxGNs2Q",
+    authDomain: "chat-bd-4d485.firebaseapp.com",
+    databaseURL: "https://chat-bd-4d485.firebaseio.com",
+    projectId: "chat-bd-4d485",
+    storageBucket: "chat-bd-4d485.appspot.com",
+    appId: "1:76008893632:web:ba30c4dbec9f1973",
+    messagingSenderId: "76008893632",
+    });
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = user => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref('messages');
+  }
+
+  parse = snapshot => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+    const timestamp = new Date(numberStamp);
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user,
+    };
+    return message;
+  };
+
+  on = callback =>
+    this.ref
+      .limitToLast(20)
+      .on('child_added', snapshot => callback(this.parse(snapshot)));
+
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+  // send the message to the Backend
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp,
+      };
+      this.append(message);
+    }
+  };
+
+  append = message => this.ref.push(message);
+
+  // close the connection to the Backend
+  off() {
+    this.ref.off();
+  }
+}
 
 Fire.shared = new Fire();
 export default Fire;
